ff3284553fb6be306cb3d253022af0191e6e2315
Jayme Woogerd <jayme.woogerd@tufts.edu>
2015-04-22 07:28:42 -0700

    Initial Commit

---
+++ yelp_clientTests/yelp_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  yelp_clientTests.swift
+//  yelp_clientTests
+//
+//  Created by Jayme Woogerd on 4/22/15.
+//  Copyright (c) 2015 Jayme Woogerd. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class yelp_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
